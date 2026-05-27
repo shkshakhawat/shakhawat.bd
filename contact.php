f668110e68f0cbb2e76231ee3fc5ce30931f2571
shkshakhawat <shakhawat.flickmedia@gmail.com>
2026-05-28 05:35:58 +0600

    Fix CORS preflight OPTIONS handling in contact form

--- contact.php
+++ contact.php
@@ -7,7 +7,13 @@ use PHPMailer\PHPMailer\Exception;
 
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
